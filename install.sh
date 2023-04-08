db5a6de6d9ec9a1a309ae06dfa1ef6d2113649f8
casjay <git-admin@casjaysdev.com>
2023-04-08 16:50:42 -0400

    🗃️  Committing everything that changed 🗃️

--- install.sh
+++ install.sh
@@ -187,6 +187,7 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # run post install scripts
 run_postinst() {
+  ln -sf "$APPDIR/starship.toml" "$HOME/.config/starship.toml"
   dfmgr_run_post
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
